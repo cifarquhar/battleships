cf66fb8fe45f3169cd2b2136f42b3e6b9e07613b
Colin Farquhar <cifarquhar@gmail.com>
2017-05-13 15:32:41 +0100

    add guard to prevent ships being added to tracking grid

--- client/src/components/Square.jsx
+++ client/src/components/Square.jsx
@@ -11,7 +11,7 @@ class Square extends React.Component{
   }
 
   handleClick(){
-    if (!this.state.full && this.props.filledSquares < 17){
+    if ((!this.state.full && this.props.filledSquares < 17) && this.props.type === "primary"){
       this.props.increaseFilledSquares()
       this.setState({full: true})
       this.setState({className: "boatSquare"})
